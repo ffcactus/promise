04d23845aa0e713f8b4e4ed21d5cf35a27a9a2b5
ffcactus <ffcactus@me.com>
2019-01-16 23:53:12 +0800

    Start eureka in container.

--- platform/registry/src/main/java/com/promise/platform/registry/RegistryApplication.java
+++ platform/registry/src/main/java/com/promise/platform/registry/RegistryApplication.java
@@ -2,10 +2,10 @@ package com.promise.platform.registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient(autoRegister = false)
+@EnableEurekaServer
 public class RegistryApplication
 {
     public static void main(String[] args)
