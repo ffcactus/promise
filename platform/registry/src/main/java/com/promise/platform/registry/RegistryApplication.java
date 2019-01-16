89d03511818220b47d360787b2ae7e01224e6a8b
ffcactus <ffcactus@me.com>
2019-01-16 23:37:55 +0800

    Try to build docker image.

--- platform/registry/src/main/java/com/promise/platform/registry/RegistryApplication.java
+++ platform/registry/src/main/java/com/promise/platform/registry/RegistryApplication.java
@@ -1,5 +1,6 @@
 package com.promise.platform.registry;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
@@ -7,5 +8,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient(autoRegister = false)
 public class RegistryApplication
 {
-
+    public static void main(String[] args)
+    {
+        SpringApplication.run(RegistryApplication.class, args);
+    }
 }
