222cfd68d42d0901e89805fe3b7bc19eb4781d7c
ffcactus <ffcactus@me.com>
2019-02-18 22:50:43 +0800

    Add styled components.

---
+++ ui/src/components/platform/widgets/Button.js
@@ -0,0 +1,19 @@
+import style, { css } from 'styled-components';
+
+const Button = style.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+
+  ${props =>
+    props.primary &&
+    css`
+      background: palevioletred;
+      color: white;
+    `};  
+`;
+
+export default Button;
