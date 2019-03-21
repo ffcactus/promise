a38e3868d3cf79a05adf81e757e622fbd82ffc34
ffcactus <ffcactus@me.com>
2019-03-21 22:26:08 +0800

    Update multi-tenant module.
    To use multiple database, you should use a single MongoClient for it
    caches database.

--- study/multi-tenant/src/main/java/com/promise/study/multitanent/MultiTenantApplication.java
+++ study/multi-tenant/src/main/java/com/promise/study/multitanent/MultiTenantApplication.java
@@ -9,12 +9,14 @@ import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
 import com.promise.study.multitanent.service.DatabaseService;
 
-@SpringBootApplication(exclude = {MongoAutoConfiguration.class, MongoDataAutoConfiguration.class})
+@SpringBootApplication(exclude = {
+        MongoAutoConfiguration.class, MongoDataAutoConfiguration.class
+})
 public class MultiTenantApplication implements CommandLineRunner
 {
-	@Autowired
-	private DatabaseService databaseService;
-	
+    @Autowired
+    private DatabaseService databaseService;
+
     public static void main(String[] args)
     {
         SpringApplication.run(MultiTenantApplication.class, args);
@@ -24,6 +26,6 @@ public class MultiTenantApplication implements CommandLineRunner
     public void run(String... args)
             throws Exception
     {
-    	databaseService.createDatabase("company5", "", "");
+        databaseService.createDatabase("company1", "", "");
     }
 }
--- study/multi-tenant/src/main/java/com/promise/study/multitanent/service/DatabaseService.java
+++ study/multi-tenant/src/main/java/com/promise/study/multitanent/service/DatabaseService.java
@@ -1,65 +1,82 @@
 package com.promise.study.multitanent.service;
 
 import org.bson.Document;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 
 import com.mongodb.MongoClient;
-import com.mongodb.MongoCredential;
 import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoDatabase;
-import com.promise.study.multitanent.domain.CompanyRoot;
 
 /**
- * Provide the capability to manipulate the database for individual companies. 
+ * Provide the capability to manipulate the database for individual companies.
  *
  */
 @Service
-public class DatabaseService {
-	private static final String ADMIN_DB = "admin";
-	private MongoOperations rootOp;
-	
-	/**
-	 * Default constructor. Since the connection to the root database is fixed, we initialize it here.
-	 */
-	public DatabaseService() {
-//		MongoClient client = new MongoClient(new ServerAddress("100.101.64.170", 27019));
-//		rootOp = new MongoTemplate(client, "root");
-	}
-	
-	/**
-	 * Create the database for a company.
-	 * @param companyId The company's ID.
-	 * @param username The username of the company's database.
-	 * @param password The password of the company's database.
-	 */
-	public void createDatabase(String databaseName, String username, String password) {
-		MongoClient client = null;
-		try {
-			client = new MongoClient(new ServerAddress("100.101.64.170", 27019));
-			MongoDatabase db = client.getDatabase(ADMIN_DB);
-			Document result = db.runCommand(new Document("enablesharding", databaseName));
-			System.out.println(result);
-		} finally {
-			if (client != null ) {
-				client.close();
-			}
-		}		
-	}
-	
-	@Cacheable("database")
-	public MongoTemplate getCompanyDatabase(String companyId) {
-		CompanyRoot companyRoot = rootOp.findById(companyId, CompanyRoot.class);
-		MongoClient client = new MongoClient(
-				new ServerAddress("100.101.64.170", 27017), 
-				MongoCredential.createCredential(
-						companyRoot.getDatabase().getUsername(), 
-						"admin", 
-						companyRoot.getDatabase().getPassword().toCharArray()), 
-				null);
-		return new MongoTemplate(client, "company");
-		
-	}
+public class DatabaseService
+{
+    private static final String ADMIN_DB = "admin";
+    private MongoOperations rootOp;
+
+    private final MongoClient rootClient;
+
+    private final MongoClient companyClient;
+
+    @Value("${db.root.host}")
+    private String rootHost;
+    @Value("${db.root.port}")
+    private int rootPort;
+    @Value("${db.company.host}")
+    private String companyHost;
+    @Value("${db.company.port}")
+    private int companyPort;
+
+    /**
+     * Default constructor. Since the connection to the root database is fixed, we
+     * initialize it here.
+     */
+    public DatabaseService()
+    {
+        rootClient = new MongoClient(new ServerAddress(rootHost, rootPort));
+        companyClient = new MongoClient(new ServerAddress(companyHost, companyPort));
+    }
+
+    /**
+     * Create the database for a company.
+     *
+     * @param companyId The company's ID.
+     * @param username  The username of the company's database.
+     * @param password  The password of the company's database.
+     */
+    public void createDatabase(String databaseName, String username, String password)
+    {
+        final MongoClient client = null;
+        try
+        {
+            final MongoDatabase db = companyClient.getDatabase(ADMIN_DB);
+            final Document result = db.runCommand(new Document("enablesharding", databaseName));
+            System.out.println(result);
+        }
+        finally
+        {
+            if (client != null)
+            {
+                client.close();
+            }
+        }
+    }
+
+    @Cacheable("database")
+    public MongoTemplate getCompanyDatabase(String companyId)
+    {
+        return new MongoTemplate(companyClient, getCompanyDatabaseName(companyId));
+    }
+
+    private String getCompanyDatabaseName(String companyId)
+    {
+        return companyId;
+    }
 }
