9ef41219f1d17a439f5f83736dc4d7814d2c343d
b00392874 <baibin3@huawei.com>
2019-03-22 17:25:43 +0800

    Mock random database access.

--- study/multi-tenant/src/main/java/com/promise/study/multitanent/MultiTenantApplication.java
+++ study/multi-tenant/src/main/java/com/promise/study/multitanent/MultiTenantApplication.java
@@ -1,5 +1,11 @@
 package com.promise.study.multitanent;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -7,13 +13,21 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
+import com.promise.study.multitanent.domain.Server;
 import com.promise.study.multitanent.service.DatabaseService;
+
+import static org.springframework.data.mongodb.core.query.Query.query;
+import static org.springframework.data.mongodb.core.query.Criteria.where;
 
 @SpringBootApplication(exclude = {
         MongoAutoConfiguration.class, MongoDataAutoConfiguration.class
 })
 public class MultiTenantApplication implements CommandLineRunner
 {
+	private static final int NUM_OF_COMPANY = 100;
+	private static final int SERVER_NUM_PER_COMPANY = 100;
+	private static final int FETCH_COUNT = 10000;
+	
     @Autowired
     private DatabaseService databaseService;
 
@@ -26,6 +40,50 @@ public class MultiTenantApplication implements CommandLineRunner
     public void run(String... args)
             throws Exception
     {
-        databaseService.createDatabase("company1", "", "");
+    	List<String> companyNames = new ArrayList<String>();
+    	for (int i = 0; i < NUM_OF_COMPANY; i++) {
+    		companyNames.add("company" + i);
+    	}
+    	
+    	// reset.
+    	for(String name : companyNames) {
+    		databaseService.removeCompanyDatabase(name);
+    	}
+    	
+    	for(String name : companyNames) {
+    		var mongoTemplate = databaseService.createCompanyDatabase(name, "", "");
+    		mongoTemplate.insertAll(createCompanyServers(name));    		
+    	}
+    	 
+    	// start test.
+    	var start = System.currentTimeMillis();
+    	for (int i = 0; i < FETCH_COUNT; i++) {
+    		
+    		var r = new Random();
+    		var companyName = "company" + r.nextInt(NUM_OF_COMPANY);
+    		var serverName = "server" + r.nextInt(SERVER_NUM_PER_COMPANY);
+    		var mongoTemplate = databaseService.getCompanyDatabase(companyName);
+    		var result = mongoTemplate.find(query(where("name").is(serverName)), Server.class);
+    		if (result.size() != 1) {
+    			System.out.printf("Failed to fetch %s in %s\n", serverName, companyName);
+    		} else {
+    			System.out.printf("Success on %s %s %d\n", serverName, companyName, i);
+    		}
+    	}
+    	var end = System.currentTimeMillis();
+    	System.out.printf("Total time = %s", (end - start) / 1000.0f);
+    	
+    }
+    
+    private Collection<Server> createCompanyServers(String companyName) {
+    	var ret = new ArrayList<Server>();
+    	for (int i = 0; i < SERVER_NUM_PER_COMPANY; i++) {
+    		var server = new Server();
+    		server.setId(UUID.randomUUID().toString());
+    		server.setName("server" + i);
+    		server.setDescription(companyName + " own this server.");
+    		ret.add(server);
+    	}
+    	return ret;
     }
 }
--- study/multi-tenant/src/main/java/com/promise/study/multitanent/service/DatabaseService.java
+++ study/multi-tenant/src/main/java/com/promise/study/multitanent/service/DatabaseService.java
@@ -2,81 +2,71 @@ package com.promise.study.multitanent.service;
 
 import org.bson.Document;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
 
 import com.mongodb.MongoClient;
 import com.mongodb.ServerAddress;
-import com.mongodb.client.MongoDatabase;
 
 /**
  * Provide the capability to manipulate the database for individual companies.
  *
  */
 @Service
-public class DatabaseService
-{
-    private static final String ADMIN_DB = "admin";
-    private MongoOperations rootOp;
+public class DatabaseService {
+	private static final String ADMIN_DB = "admin";
 
-    private final MongoClient rootClient;
+	private MongoOperations rootOp;
 
-    private final MongoClient companyClient;
+	private MongoClient rootClient;
 
-    @Value("${db.root.host}")
-    private String rootHost;
-    @Value("${db.root.port}")
-    private int rootPort;
-    @Value("${db.company.host}")
-    private String companyHost;
-    @Value("${db.company.port}")
-    private int companyPort;
+	private MongoClient companyClient;
 
-    /**
-     * Default constructor. Since the connection to the root database is fixed, we
-     * initialize it here.
-     */
-    public DatabaseService()
-    {
+	@Value("${db.root.host}")
+	private String rootHost;
+	@Value("${db.root.port}")
+	private int rootPort;
+	@Value("${db.company.host}")
+	private String companyHost;
+	@Value("${db.company.port}")
+	private int companyPort;
+
+	/**
+	 * Default constructor. Since the connection to the root database is fixed, we
+	 * initialize it here.
+	 */
+	public DatabaseService(@Value("${db.root.host}") String rootHost, @Value("${db.root.port}") int rootPort,
+			@Value("${db.company.host}") String companyHost, @Value("${db.company.port}") int companyPort) {
         rootClient = new MongoClient(new ServerAddress(rootHost, rootPort));
         companyClient = new MongoClient(new ServerAddress(companyHost, companyPort));
-    }
+	}
 
-    /**
-     * Create the database for a company.
-     *
-     * @param companyId The company's ID.
-     * @param username  The username of the company's database.
-     * @param password  The password of the company's database.
-     */
-    public void createDatabase(String databaseName, String username, String password)
-    {
-        final MongoClient client = null;
-        try
-        {
-            final MongoDatabase db = companyClient.getDatabase(ADMIN_DB);
-            final Document result = db.runCommand(new Document("enablesharding", databaseName));
-            System.out.println(result);
-        }
-        finally
-        {
-            if (client != null)
-            {
-                client.close();
-            }
-        }
-    }
+	/**
+	 * Create the database for a company.
+	 *
+	 * @param companyId The company's ID.
+	 * @param username  The username of the company's database.
+	 * @param password  The password of the company's database.
+	 */
+	public MongoTemplate createCompanyDatabase(String companyId, String username, String password) {
 
-    @Cacheable("database")
-    public MongoTemplate getCompanyDatabase(String companyId)
-    {
-        return new MongoTemplate(companyClient, getCompanyDatabaseName(companyId));
-    }
+		final var db = companyClient.getDatabase(ADMIN_DB);
+		final var databaseName = getCompanyDatabaseName(companyId);
+		final var result = db.runCommand(new Document("enablesharding", databaseName));
+		System.out.println(result);
+		return new MongoTemplate(companyClient, databaseName);
+	}
 
-    private String getCompanyDatabaseName(String companyId)
-    {
-        return companyId;
-    }
+	public MongoTemplate getCompanyDatabase(String companyId) {
+		return new MongoTemplate(companyClient, getCompanyDatabaseName(companyId));
+	}
+
+	public void removeCompanyDatabase(String companyId) {
+		getCompanyDatabase(companyId).getDb().drop();
+	}
+
+	private String getCompanyDatabaseName(String companyId) {
+		return companyId;
+	}
 }
